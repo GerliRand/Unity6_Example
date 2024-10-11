a0ca73936018c67435f49ad79c6f1ad312ac2278
Gerli Rand <gerli@hotmail.fi>
2024-10-11 18:37:20 +0300

    Lisätty liikumiselle animaatiot: seis - Hengailu1 ja liikkuu - Juoksu

--- Assets/Scripts/PekkaLiike.cs
+++ Assets/Scripts/PekkaLiike.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class PekkaLiike : MonoBehaviour
 {
     CharacterController pekkaKontrolleri;
+    Animation pekkaAnimaatiot;
     Vector3 pelaajanMaanvetovoima;
 
     float maanvetovoima = -9.81f;
@@ -13,11 +14,16 @@ public class PekkaLiike : MonoBehaviour
     void Start()
     {
         pekkaKontrolleri = GetComponent<CharacterController>();
+        pekkaAnimaatiot = GetComponent<Animation>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Hahmo käänty oikealle ja vasemalle (nuolinäppäimet tai a/d)
+        float sivuttaisliike = Input.GetAxis("Horizontal");
+        transform.Rotate(0f, sivuttaisliike, 0f);
+
         pelaajaOnMaassa = pekkaKontrolleri.isGrounded;
         if (pelaajaOnMaassa && pelaajanMaanvetovoima.y < 0)
         {
@@ -26,6 +32,19 @@ public class PekkaLiike : MonoBehaviour
 
         // Liikkuu eteen- ja taaksepäin, toimi nuoli- sekä w/s-näppämillä
         float liikeEteenpain = Input.GetAxis("Vertical");
+
+        // jos eteenpäin liike on 0 - Hengailu1 animaatio 
+        // jos eteenpäinliike on jotain muuta kuin 0 - Juoksu animaatio
+        if (liikeEteenpain == 0f)
+        {
+            pekkaAnimaatiot.Play("Hengailu1");
+        }
+        else
+        {
+            pekkaAnimaatiot.Play("Juoksu");
+        }
+
+
         //transform.position = transform.position + (transform.forward * 0.01f * liikeEteenpain);
         pekkaKontrolleri.Move(transform.forward * 0.01f * liikeEteenpain);
 
